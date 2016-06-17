4a2883bed88777fb0db9e3b178bb32a1608847e2
travy3 <zyu.lolee@gmail.com>
2016-06-18 06:57:39 +0800

    add housework

--- advance.py
+++ advance.py
@@ -1,5 +1,65 @@
 from collections import Iterator
 
+import os
+from collections import Iterable
+from this import s
+
+L = []
+n = 1
+while n <=99:
+    L.append(n)
+    n = n +2
+
+print(L)
+
+#切片(Slice) [::] 类似于截取函数substring
+L = ['Michael', 'Sarah', 'Tracy', 'Bob', 'Jack']
+
+print(L[1:3])
+
+print(L[0:5:2])
+
+L = list(range(100))
+print(L)
+print(L[:10])
+print(L[-20::2])
+print(L[::5])
+print(L[:])
+arg = (0,1,2,3,4,5)
+
+print(arg[:3])
+
+#迭代
+d = {'a': 1, 'b': 2, 'c': 3}
+
+for value in d.values():
+    print(value)
+
+for k,v in d.items():
+    print(k,v)
+
+print(isinstance([1,2,3],Iterable))
+
+# 把list变为索引-元素对
+for i,value in enumerate(['a','b','c']):
+    print(i,value)
+
+#列表生成式
+print(list(range(1,11)))
+l = []
+for x in range(1,11):
+    l.append(x*x)
+print(l)
+print([x * x for x in range(1,11)])
+print([x * x for x in range(1,11) if x % 2 == 0])
+print([m + n for m in 'ABC' for n in 'abc'])
+
+print(d for d in os.listdir('.'))
+
+L = ['Hello', 'World', 'IBM', 'Apple']
+
+print([s.lower() for s in L])
+
 L1 = ['Hello', 'World', 18, 'Apple', None]
 L2 = [x.lower() for x in L1 if isinstance(x,str)]
 print(L2)
